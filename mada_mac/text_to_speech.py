db7dbd9c359db15a187ac1deaf68ca4816e6323c
Alberto Julián <ajulianes@yahoo.es>
2024-09-29 19:22:19 +0200

    class TTS removed

--- mada_mac/text_to_speech.py
+++ mada_mac/text_to_speech.py
@@ -45,56 +45,12 @@ def audiofile_to_speech(audio_file, audio_speed=1.75):
 
 
 def create_gtts_connection_error_audio():
+
     message = "gtts connection error. Check if the Mac is connected to internet"
     gtts_ok = text_to_speech(message)
     if gtts_ok:
         import shutil
         shutil.copy("tts_out.mp3", "gtts_connection_error.mp3")
-
-
-class TTS:
-    def __init__(self):
-        self.audio_speed = 1.75
-
-    def text_to_speech(self, message, audio_speed=1.75, print_message=True, audio_file="tts_out.mp3"):
-
-        if print_message:
-            print("\n<<<<<<<<<<< Start printing audio message: ")
-            print(message)
-            print(">>>>>>>>>>>>>> End printing audio message\n")
-
-        # Create the gTTS object with the message
-        # tts = gTTS(message, lang='es')
-        tts = gTTS(message, lang='en')
-
-        # Save the audio file with the message
-        try:
-            tts.save(audio_file)
-
-            self.audiofile_to_speech(audio_file, audio_speed)
-
-            return True
-
-        except gtts.gTTSError:
-            print("#######################################")
-            print("gTTS Connection error; check if the Mac is connected to internet (AndroidAJR)")
-            print("#######################################")
-
-            connection_error_audio_file = "gtts_connection_error.mp3"
-            self.audiofile_to_speech(connection_error_audio_file, audio_speed)
-
-    def audiofile_to_speech(self, audio_file, audio_speed=None):
-
-        # play the file with the message with Apple's afplay command
-        # Original voices are slow => increase with the -r parameter in afplay
-        # os.system(f"afplay {audio_file} -r {audio_speed}")
-        if audio_speed is None:
-            audio_speed = self.audio_speed
-
-        if os.path.exists(audio_file):
-            subprocess.run(f"afplay {audio_file} -r {audio_speed}", shell=True)
-        else:
-            print(f"Cannot find file {audio_file}")
 
 
 def main1():
